e3884c21236c47260512bddeead124df0532ebb3
katherine fernandez <katherinefernandez@katherines-MacBook-Pro.local>
2021-03-13 17:44:08 -0500

    added footer

--- src/page/Home.js
+++ src/page/Home.js
@@ -3,6 +3,7 @@ import "../App.css";
 import NewReleases from "../components/NewReleases/NewReleases";
 import ActionAdventure from "../components/ActionAdventure/ActionAdventure";
 import ScifiHorror from "../components/ScifiHorror/ScifiHorror";
+import Footer from "../components/Footer/Footer";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <NewReleases />
       <ActionAdventure />
       <ScifiHorror />
+      <Footer />
     </div>
   );
 };
