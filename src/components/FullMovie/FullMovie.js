76d9ec941a492dd9cea97d134e0afc1b04b6350e
Ayman Omer <aymangebrilomer@gmail.com>
2021-03-15 01:13:34 -0400

    just fixing some pugs

--- src/components/FullMovie/FullMovie.js
+++ src/components/FullMovie/FullMovie.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./FullMovie.css";
 
 const FullMovie = ({ movieId }) => {
   const [fullMoviebanar, setFullMoviebanar] = useState([]);
@@ -17,37 +18,42 @@ const FullMovie = ({ movieId }) => {
   };
 
   return (
-    <>
-      <header
-        className="banar"
-        style={{
-          backgroundSize: "cover",
-          backgroundImage: `url("https://image.tmdb.org/t/p/original${fullMoviebanar?.backdrop_path}")`,
-          backgroundPosition: "center center",
-        }}
-      >
-        <div className="banar-info">
-          <h1 className="banar-title">
-            {fullMoviebanar?.title ||
-              fullMoviebanar?.name ||
-              fullMoviebanar?.orignal_name}
-          </h1>
-          <div className="banar-buttons">
-            <button
-              className="banar-button"
-              // onClick={() => handleClickMovie(fullMoviebanar)}
-            >
-              Play
-            </button>
+    <div className="app">
+      <>
+        <header
+          className="banar"
+          style={{
+            backgroundSize: "cover",
+            backgroundImage: `url("https://image.tmdb.org/t/p/original${fullMoviebanar?.backdrop_path}")`,
+            backgroundPosition: "center center",
+          }}
+        >
+          <div className="banar-info">
+            <h1 className="banar-title">
+              {fullMoviebanar?.title ||
+                fullMoviebanar?.name ||
+                fullMoviebanar?.orignal_name}
+            </h1>
+            <div className="banar-buttons">
+              <button
+                className="banar-button"
+                // onClick={() => handleClickMovie(fullMoviebanar)}
+              >
+                Play
+              </button>
+            </div>
+            <h1 className="banar-description">
+              {truncate(fullMoviebanar?.overview, 150)}
+            </h1>
           </div>
-          <h1 className="banar-description">
-            {truncate(fullMoviebanar?.overview, 150)}
-          </h1>
-        </div>
-        <div className="banar-fadeBottom"></div>
-      </header>
-      <div className="full-movie-dea"></div>
-    </>
+          <div className="banar-fadeBottom"></div>
+        </header>
+      </>
+      <div className="full-movie-dea">
+        <h3> Movie Title : {fullMoviebanar.original_title}</h3>
+        <h3>release Date :{fullMoviebanar.release_date}</h3>
+      </div>
+    </div>
   );
 };
 
